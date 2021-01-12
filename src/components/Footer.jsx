d87027c456758879b3078dab30d2a9655ecf574f
Bharath <bharaht.janjanam98@gmail.com>
2021-01-12 19:45:10 +0530

    commit changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Footer(){
+    <div>
+
+    </div>    
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header (){
+    return (
+        <div>
+            <h1>My Name is Bharath kumar Janjanam</h1>
+            <h2>HELLO WORLD</h2>
+        </div>
+    );
+}
+export default Header;
