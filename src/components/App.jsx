6396c5838dcc73078a772cf0661495ea7fec7f24
Bharath <bharaht.janjanam98@gmail.com>
2021-01-12 19:01:00 +0530

    added components folder

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import "./MyInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <MyInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyInfo (){
+    return (
+        <div>
+            <h1>My Name is Bharath kumar Janjanam</h1>
+            <h2>HELLO WORLD</h2>
+        </div>
+    );
+}
+export default MyInfo;
